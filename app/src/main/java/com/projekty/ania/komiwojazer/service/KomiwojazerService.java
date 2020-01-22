a709e8708d56828fe04162316d969ad303fe17e0
Aneczka <anlukasiewicz95@gmail.com>
2020-01-22 23:15:15 +0100

    Updated version

--- app/src/main/java/com/projekty/ania/komiwojazer/service/KomiwojazerService.java
+++ app/src/main/java/com/projekty/ania/komiwojazer/service/KomiwojazerService.java
@@ -3,27 +3,27 @@ package com.projekty.ania.komiwojazer.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-
 import androidx.annotation.Nullable;
 
-import com.projekty.ania.komiwojazer.ITSPService;
+import com.projekty.ania.komiwojazer.aidlService;
 
 public class KomiwojazerService extends Service {
 
-    // https://www.sitepoint.com/aidl-for-sharing-functionality-between-android-apps/?fbclid=IwAR0fYrVRmAz6Bp8FilmK-SJaCUV7o1YSX0TynsIs04ovT1MrPfQ_jx4KhQw
-
     private int[][] graph;
-    private boolean[] v; // to check if node has been visited or not
+    private boolean[] nodeVisited;
 
-    private final ITSPService.Stub mBinder = new ITSPService.Stub() {
+    //https://www.sitepoint.com/aidl-for-sharing-functionality-between-android-apps/?fbclid=IwAR0fYrVRmAz6Bp8FilmK-SJaCUV7o1YSX0TynsIs04ovT1MrPfQ_jx4KhQw
+
+
+    private final aidlService.Stub mBinder = new aidlService.Stub() {
+
         @Override
-        public int getResult() {
+        public int getProduct() {
             int ans = Integer.MAX_VALUE;
-            int n = graph.length; //number of nodes
-            // Mark 0th node as visited
-            v[0] = true;
-            // Find the minimum weight Hamiltonian Cycle
-            return tsp(graph, v, 0, n, 1, 0, ans);
+            int nodesNumber = graph.length;
+            nodeVisited[0] = true;
+
+            return hc(graph, nodeVisited, 0, nodesNumber, 1, 0, ans);
         }
     };
 
@@ -36,28 +36,27 @@ public class KomiwojazerService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        graph = new int[][]{{0, 10, 15, 20},
-                {10, 0, 35, 25},
-                {15, 35, 0, 30},
-                {20, 25, 30, 0}};
+        graph = new int[][]{
+                {0, 4, 1, 9},
+                {3, 0, 6, 11},
+                {4, 1, 0, 2},
+                {6, 5, -4, 0}};
 
-
-        v = new boolean[graph.length];
+        nodeVisited = new boolean[graph.length];
     }
 
     //https://www.geeksforgeeks.org/travelling-salesman-problem-implementation-using-backtracking/
-    // Function to find the minimum weight
-    // Hamiltonian Cycle
-    private int tsp(int[][] graph, boolean[] v, int currPos, int n, int count, int cost, int ans) {
+
+    private int hc(int[][] graph, boolean[] v, int currentPosition, int n, int count, int cost, int result) {
 
         // If last node is reached and it has a link
         // to the starting node i.e the source then
         // keep the minimum value out of the total cost
         // of traversal and "ans"
         // Finally return to check for more possible values
-        if (count == n && graph[currPos][0] > 0) {
-            ans = Math.min(ans, cost + graph[currPos][0]);
-            return ans;
+        if (count == n && graph[currentPosition][0] > 0) {
+            result = Math.min(result, cost + graph[currentPosition][0]);
+            return result;
         }
 
         // BACKTRACKING STEP
@@ -65,18 +64,18 @@ public class KomiwojazerService extends Service {
         // of currPos node and increasing the count
         // by 1 and cost by graph[currPos,i] value
         for (int i = 0; i < n; i++) {
-            if (v[i] == false && graph[currPos][i] > 0) {
+            if (v[i] == false && graph[currentPosition][i] > 0) {
 
                 // Mark as visited
                 v[i] = true;
-                ans = tsp(graph, v, i, n, count + 1,
-                        cost + graph[currPos][i], ans);
+                result = hc(graph, v, i, n, count + 1,
+                        cost + graph[currentPosition][i], result);
 
                 // Mark ith node as unvisited
                 v[i] = false;
             }
         }
-        return ans;
+        return result;
     }
 
 }
